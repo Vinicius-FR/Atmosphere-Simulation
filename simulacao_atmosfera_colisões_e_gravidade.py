582f0dbe0c4c757ed452310e09d0f72cf19b7845
Vinícius FR <viniciusfr44a@gmail.com>
2023-06-25 21:25:48 -0300

    refatorando o código para corrigir condições iniciais de velocidades aleatórias, agitação e colisões, além de adicionar as imagens de alguns resultados iniciais

--- simulacao_atmosfera_colisões_e_gravidade.py
+++ simulacao_atmosfera_colisões_e_gravidade.py
@@ -2,60 +2,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parâmetros da simulação
-num_particulas = 1000  # Número de partículas na simulação
+num_particulas = 1000  # Número de partículas
 dimensao = 2  # Dimensão do espaço (2D ou 3D)
 num_passos = 1000  # Número de passos da simulação
-dt = 0.1  # Intervalo de tempo entre os passos da simulação
+dt = 0.1  # Intervalo de tempo entre os passos
 
 # Parâmetros físicos
-gravidade = np.array([0, -9.8])  # Vetor de gravidade
+gravidade = np.array([0, -9.8])  # Vetor gravidade
 coef_restituicao = 1  # Coeficiente de restituição para colisões
 
-# Limites da região de simulação
+# Limites da região
 limite_x = [0, 10]
 limite_y = [0, 10]
 
 # Inicialização das posições e velocidades
 posicoes = np.random.uniform(low=[limite_x[0], limite_y[0]], high=[limite_x[1], limite_y[1]], size=(num_particulas, dimensao))
-velocidades = np.zeros((num_particulas, dimensao))
+velocidade_magnitude = 10.0  # Valor da magnitude da velocidade
+phi = np.random.uniform(0, 2*np.pi, size=num_particulas)  # Ângulos aleatórios
+theta = np.random.uniform(0, np.pi, size=num_particulas)
+velocidades = velocidade_magnitude * np.vstack((np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi))).T
 
 # Simulação
 for t in range(num_passos):
-    # Cálculo da força de gravidade em cada partícula
-    forca_gravidade = gravidade
-    
-    # Cálculo da força de agitação aleatória em cada partícula
-    forca_agitacao = np.random.randn(num_particulas, dimensao)
+    # Cálculo da aceleração devido a uma agitação aleatória em cada partícula
+    agitacao = 10.0 * np.random.randn(num_particulas, dimensao)
     
     # Cálculo da aceleração resultante em cada partícula
-    aceleracoes = forca_gravidade + forca_agitacao
+    aceleracao = gravidade + agitacao
     
     # Atualização das velocidades e posições usando o método de Euler
-    velocidades += aceleracoes * dt
+    velocidades += aceleracao * dt
     posicoes += velocidades * dt
     
     # Verificação e tratamento de colisões com as paredes
     for i in range(num_particulas):
         for j in range(dimensao):
-            if posicoes[i, j] < limite_x[0]:
-                posicoes[i, j] = limite_x[0]
-                velocidades[i, j] *= -coef_restituicao
-            elif posicoes[i, j] > limite_x[1]:
-                posicoes[i, j] = limite_x[1]
-                velocidades[i, j] *= -coef_restituicao
-            if posicoes[i, j] < limite_y[0]:
-                posicoes[i, j] = limite_y[0]
-                velocidades[i, j] *= -coef_restituicao
-            elif posicoes[i, j] > limite_y[1]:
-                posicoes[i, j] = limite_y[1]
-                velocidades[i, j] *= -coef_restituicao
+            if posicoes[i, 0] < limite_x[0]:
+                posicoes[i, 0] = limite_x[0]
+                velocidades[i, 0] *= -coef_restituicao
+            elif posicoes[i, 0] > limite_x[1]:
+                posicoes[i, 0] = limite_x[1]
+                velocidades[i, 0] *= -coef_restituicao
+            if posicoes[i, 1] < limite_y[0]:
+                posicoes[i, 1] = limite_y[0]
+                velocidades[i, 1] *= -coef_restituicao
+            elif posicoes[i, 1] > limite_y[1]:
+                posicoes[i, 1] = limite_y[1]
+                velocidades[i, 1] *= -coef_restituicao
     
     # Verificação e tratamento de colisões entre as partículas
     for i in range(num_particulas):
         for j in range(i+1, num_particulas):
             distancia = np.linalg.norm(posicoes[i] - posicoes[j])
-            if distancia < 1.0:  # Defina o limite de distância para detectar colisões
-                # Cálculo do vetor de direção entre as partículas
+            if distancia < 0.1:  # Limite de distância para detectar colisões (depende da dimensão e quantidade de partículas)
+                # Cálculo do versor de direção entre as partículas
                 if distancia == 0:
                     direcao = np.zeros(dimensao)
                 else:
@@ -65,7 +65,7 @@ for t in range(num_passos):
                 # Cálculo das velocidades relativas entre as partículas
                 velocidade_relativa = np.dot(velocidades[j] - velocidades[i], direcao)
                 
-                # Cálculo das forças de colisão
+                # Cálculo da variação de momento/velocidade devido à colisão
                 forca_colisao = coef_restituicao * velocidade_relativa * direcao
                 
                 # Atualização das velocidades
@@ -73,11 +73,12 @@ for t in range(num_passos):
                 velocidades[j] -= forca_colisao
     
     # Visualização da simulação
-    plt.figure()
-    plt.scatter(posicoes[:, 0], posicoes[:, 1])
-    plt.xlim(limite_x[0], limite_x[1])
-    plt.ylim(limite_y[0], limite_y[1])
-    plt.xlabel('Posição X')
-    plt.ylabel('Posição Y')
-    plt.title('Simulação da Atmosfera - Passo {}'.format(t+1))
-    plt.show()
+    if t % 50 == 0:
+        plt.figure()
+        plt.scatter(posicoes[:, 0], posicoes[:, 1])
+        plt.xlim(limite_x[0], limite_x[1])
+        plt.ylim(limite_y[0], limite_y[1])
+        plt.xlabel('Posição X')
+        plt.ylabel('Posição Y')
+        plt.title('Simulação da Atmosfera - Passo {}'.format(t+1))
+        plt.show()
